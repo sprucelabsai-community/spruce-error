21aecd30dc21d06188a57980bc6d3bd3f14424d6
liquidg3 <me@taylorrome.ro>
2020-03-27 15:10:48 -0600

    lint

--- src/SpruceError.ts
+++ src/SpruceError.ts
@@ -1,18 +1,16 @@
-import { ISpruceErrorOptions, SpruceErrorMap, ISpruceErrorMap } from "./types";
+import { ISpruceErrorOptions, SpruceErrorMap, ISpruceErrorMap } from './types'
 
 export default class SpruceError<
 	M extends ISpruceErrorMap = SpruceErrorMap,
 	C extends keyof M = keyof M,
 	T extends ISpruceErrorOptions<M, C> = ISpruceErrorOptions<M, C>
-	> extends Error {
-
-	public readonly code: C;
-	public options?: T;
+> extends Error {
+	public readonly code: C
+	public options?: T
 
 	public constructor(code: C, options?: T) {
-		super(code as string);
-		this.code = code;
-		this.options = options;
+		super(code as string)
+		this.code = code
+		this.options = options
 	}
 }
-
--- src/types.ts
+++ src/types.ts
@@ -5,41 +5,43 @@ export interface ISpruceErrorContext {}
 
 // all error codes get dropped into this enum
 export enum SpruceErrorCode {
-    UnknownError = 'UNKNOWN_ERROR',
-    MissingParameters = 'MISSING_PARAMETERS',
-    InvalidParameters = 'INVALID_PARAMATERS'
+	UnknownError = 'UNKNOWN_ERROR',
+	MissingParameters = 'MISSING_PARAMETERS',
+	InvalidParameters = 'INVALID_PARAMATERS'
 }
 
 /** parent interface for all error options */
-export interface ISpruceErrorOptions<M extends ISpruceErrorMap = SpruceErrorMap, T extends keyof M = keyof M> {
-    /** an easy to read version of the error */
-    friendlyReason?:string
-    /** tracking the error we caught previously if one exists  */
-    lastError?: Error
+export interface ISpruceErrorOptions<
+	M extends ISpruceErrorMap = SpruceErrorMap,
+	T extends keyof M = keyof M
+> {
+	/** an easy to read version of the error */
+	friendlyReason?: string
+	/** tracking the error we caught previously if one exists  */
+	lastError?: Error
 	/** The HTTP status code that most closely corresponds to this error: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status */
 	httpStatusCode?: HttpStatusCode
 	/** the context with this error  */
-    context: M[T] 
+	context: M[T]
 }
 
 /** mapping of codes to options */
 export interface ISpruceErrorMap {
-    [code: string]: ISpruceErrorContext
+	[code: string]: ISpruceErrorContext
 }
 
 /** a map of all errors to their context */
 export type SpruceErrorMap = {
-    /** we're not sure what happened */
-    [SpruceErrorCode.UnknownError]:  {}
+	/** we're not sure what happened */
+	[SpruceErrorCode.UnknownError]: {}
 
-    /** missing params */
-    [SpruceErrorCode.MissingParameters]: {
-        missingParamaters: string[]
-    }    
+	/** missing params */
+	[SpruceErrorCode.MissingParameters]: {
+		missingParamaters: string[]
+	}
 
-    /** invalid params */
-    [SpruceErrorCode.InvalidParameters]: {
-        missingParamaters: string[]
-    }
-
+	/** invalid params */
+	[SpruceErrorCode.InvalidParameters]: {
+		missingParamaters: string[]
+	}
 }
