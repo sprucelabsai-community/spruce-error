74772013e39ca127abe5d97a7e508dc4fc1db59e
liquidg3 <me@taylorrome.ro>
2020-04-03 12:14:06 -0600

    pass lint, no dependencies

--- index.ts
+++ index.ts
@@ -3,10 +3,3 @@ import AbstractSpruceError from './src/AbstractSpruceError'
 export default AbstractSpruceError
 
 export * from './src/types'
-import { ISchemaDefinition } from '@sprucelabs/schema'
-
-export function buildErrorDefinition<T extends ISchemaDefinition>(
-	definition: T
-): T {
-	return definition
-}
