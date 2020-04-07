3c8d3985db9be71357733e36e1330e64f90e6fd6
liquidg3 <me@taylorrome.ro>
2020-04-07 07:55:51 -0600

    stack utils

--- src/BaseSpruceError.ts
+++ src/BaseSpruceError.ts
@@ -1,4 +1,9 @@
 import { SpruceErrorOptions, ISpruceErrorOptions } from './types'
+import StackUtils from 'stack-utils'
+const stack = new StackUtils({
+	cwd: process.cwd(),
+	internals: StackUtils.nodeInternals()
+})
 
 export default class BaseSpruceError<
 	T extends ISpruceErrorOptions = SpruceErrorOptions
@@ -18,10 +23,16 @@ export default class BaseSpruceError<
 		}
 
 		this.message = this.friendlyMessage()
+		this.stack = this.cleanStack()
 	}
 
 	/** Get a nice, readable version of the error. subclasses extend this */
 	public friendlyMessage(): string {
 		return this.options.friendlyMessage || this.message
+	}
+
+	/** Gets you a clean version of the track stack without all the extra node bs */
+	public cleanStack() {
+		return stack.clean(this.stack)
 	}
 }
