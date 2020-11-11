91c87753791da150542d77b13f21df2679f6eed8
liquidg3 <me@taylorrome.ro>
2020-11-11 13:52:01 -0700

    major: no http status code

--- src/error.types.ts
+++ src/error.types.ts
@@ -1,19 +1,9 @@
-import { HttpStatusCode } from './lib/httpStatusCodes'
-
-// TODO find why `any` is required in Options generic
-/** Parent interface for all error options */
 export interface ISpruceErrorOptions {
-	/** Error code in CONST_CASE */
 	code: string
-	/** An easy to read version of the error */
 	friendlyMessage?: string
-	/** Tracking the error we caught previously if one exists  */
 	originalError?: Error
-	/** The HTTP status code that most closely corresponds to this error: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status */
-	httpStatusCode?: HttpStatusCode
 }
 
-/** Error options */
 export type SpruceErrorOptions =
 	| ISpruceErrorUnknownError
 	| ISpruceErrorMissingParameters
