57e367ca0dab5d6a37e61bfa75a40a240ffcc171
Taylor Romero <me@taylorrome.ro>
2020-12-28 10:03:39 -0700

    patch: don't include stack in object if stack is not truthy

--- src/__tests__/behavioral/SpruceError.test.ts
+++ src/__tests__/behavioral/SpruceError.test.ts
@@ -5,12 +5,50 @@ class SpruceError extends AbstractSpruceError {}
 
 export default class SpruceErrorTests {
 	@test()
-	public async createError() {
-		const spruceError = new SpruceError({
+	public createError() {
+		const error = new SpruceError({
 			code: 'INVALID_PARAMETERS',
 			parameters: ['test'],
 		})
 
-		assert.isTruthy(spruceError)
+		assert.isTruthy(error)
+	}
+
+	@test()
+	protected errorToObjectIncludesStack() {
+		const error = new SpruceError({
+			code: 'MISSING_PARAMETERS',
+			parameters: ['test'],
+		})
+
+		const obj = error.toObject()
+		assert.isTruthy(obj.stack)
+	}
+
+	@test()
+	protected errorToObjectWithoutStackHasNoStackProp() {
+		const error = new SpruceError({
+			code: 'MISSING_PARAMETERS',
+			parameters: ['test'],
+		})
+
+		delete error.stack
+
+		const obj = error.toObject()
+		assert.isFalse('stack' in obj)
+	}
+
+	@test()
+	protected errorToObjectWithoutStackHasNoFalseStackProp() {
+		const error = new SpruceError({
+			code: 'MISSING_PARAMETERS',
+			parameters: ['test'],
+		})
+
+		//@ts-ignore
+		error.stack = false
+
+		const obj = error.toObject()
+		assert.isFalse('stack' in obj)
 	}
 }
