73dc2dcc4ee9be645e621941d37affe15999852c
Tay <me@taylorrome.ro>
2021-08-21 07:53:50 -0600

    patch: restore error options

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export * from './src/AbstractSpruceError'
 import AbstractSpruceError from './src/AbstractSpruceError'
 export default AbstractSpruceError
+export { ErrorOptions } from './src/error.options'
